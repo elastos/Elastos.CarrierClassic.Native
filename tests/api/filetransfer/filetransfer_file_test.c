742742c7a592dad83705b3ebca7e702c6eb10a68
kunshanyu <xukunyu@elastos.org>
2019-01-23 18:06:12 +0800

    Add some file-transfer-related test cases.

--- tests/api/filetransfer/filetransfer_file_test.c
+++ tests/api/filetransfer/filetransfer_file_test.c
@@ -75,6 +75,17 @@ static ElaCallbacks callbacks = {
     .friend_invite   = NULL
 };
 
+static void ft_connect_cb(ElaCarrier *carrier,
+                          const char *address,
+                          const ElaFileTransferInfo *fileinfo,
+                          void *context)
+{
+    TestContext *wtxt = (TestContext*)context;
+    CarrierContext *ctx = wtxt->carrier;
+
+    cond_signal(ctx->ft_cond);
+}
+
 static void ft_state_changed_cb(FileTransferConnection state, void *context)
 {
     TestContext *wtxt = (TestContext*)context;
@@ -94,11 +105,14 @@ static void sent_cb(size_t length, uint64_t totalsz, void *context)
         cond_signal(ctx->ft_cond);
 }
 
-static ElaFileProgressCallbacks fp_calllbacks = {
-    .state_changed = ft_state_changed_cb,
-    .sent = sent_cb,
-    .received = NULL
-};
+static void received_cb(size_t length, uint64_t totalsz, void *context)
+{
+    TestContext *wtxt = (TestContext *)context;
+    CarrierContext *ctx = wtxt->carrier;
+
+    if (length == totalsz)
+        cond_signal(ctx->ft_cond);
+}
 
 struct CarrierContextExtra {
     char file_name[ELA_MAX_FILE_NAME_LEN + 1];
@@ -145,10 +159,11 @@ static void test_filetransfer_file(void)
 {
     CarrierContext *wctxt = test_context.carrier;
     CarrierContextExtra *extra = wctxt->extra;
+    ElaFileProgressCallbacks fp_callbacks = {0};
+    FILE *fp = NULL;
     char userid[ELA_MAX_ID_LEN + 1] = {0};
     char cmd[32] = {0};
     char result[32] = {0};
-    char *p;
     const char *data = "hello";
     uint8_t ft_con_state_bits = 0;
     int rc;
@@ -160,9 +175,11 @@ static void test_filetransfer_file(void)
     CU_ASSERT_TRUE_FATAL(ela_is_friend(wctxt->carrier, robotid));
 
     // Create a file to transfer.
-    sprintf(result, "printf %s > %s", data, extra->file_name);
-    rc = system(result);
-    CU_ASSERT_EQUAL_FATAL(rc, 0);
+    fp = fopen(extra->file_name, "w+b");
+    CU_ASSERT_PTR_NOT_NULL_FATAL(fp);
+    rc = fputs(data, fp);
+    fclose(fp);
+    CU_ASSERT_NOT_EQUAL_FATAL(rc, EOF);
 
     rc = ela_filetransfer_init(wctxt->carrier, NULL, &test_context);
     CU_ASSERT_EQUAL(rc, 0);
@@ -175,7 +192,9 @@ static void test_filetransfer_file(void)
     CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_init") == 0);
     CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
 
-    rc = ela_file_send(wctxt->carrier, robotid, extra->file_name, &fp_calllbacks, &test_context);
+    fp_callbacks.state_changed = ft_state_changed_cb;
+    fp_callbacks.sent = sent_cb;
+    rc = ela_file_send(wctxt->carrier, robotid, extra->file_name, &fp_callbacks, &test_context);
     CU_ASSERT_EQUAL(rc, 0);
 
     rc = read_ack("%32s %32s", cmd, result);
@@ -214,13 +233,131 @@ static void test_filetransfer_file(void)
     CU_ASSERT_TRUE_FATAL(strcmp(cmd, "fp_recv") == 0);
     CU_ASSERT_TRUE_FATAL(strcmp(result, "done") == 0);
 
-    rc = write_cmd("ft_res\n");
+    rc = write_cmd("ft_result\n");
     CU_ASSERT_FATAL(rc > 0);
 
     rc = read_ack("%32s %32s", cmd, result);
     CU_ASSERT_TRUE_FATAL(rc == 2);
-    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_res") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_result") == 0);
     CU_ASSERT_TRUE_FATAL(strcmp(result, data) == 0);
+
+    rc = write_cmd("ft_cleanup\n");
+    CU_ASSERT_FATAL(rc > 0);
+
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_cleanup") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
+
+    ela_filetransfer_cleanup(wctxt->carrier);
+
+    // Delete the file created before.
+    rc = remove(extra->file_name);
+    CU_ASSERT_EQUAL(rc, 0);
+}
+
+static void test_filetransfer_receive_file(void)
+{
+    CarrierContext *wctxt = test_context.carrier;
+    CarrierContextExtra *extra = wctxt->extra;
+    ElaFileProgressCallbacks fp_callbacks = {0};
+    FILE *fp = NULL;
+    char userid[ELA_MAX_ID_LEN + 1] = {0};
+    char cmd[32] = {0};
+    char result[32] = {0};
+    char path[PATH_MAX] = {0};
+    char *p;
+    const char *data = "hello";
+    uint8_t ft_con_state_bits = 0;
+    int rc;
+
+    test_context.context_reset(&test_context);
+
+    rc = add_friend_anyway(&test_context, robotid, robotaddr);
+    CU_ASSERT_EQUAL_FATAL(rc, 0);
+    CU_ASSERT_TRUE_FATAL(ela_is_friend(wctxt->carrier, robotid));
+
+    // Tell the robot to create a file to transfer.
+    rc = write_cmd("ft_file %s\n", data);
+    CU_ASSERT_FATAL(rc > 0);
+
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_file") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
+
+    rc = ela_filetransfer_init(wctxt->carrier, ft_connect_cb, &test_context);
+    CU_ASSERT_EQUAL(rc, 0);
+
+    rc = write_cmd("ft_init\n");
+    CU_ASSERT_FATAL(rc > 0);
+
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_init") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
+
+    ela_get_userid(wctxt->carrier, userid, sizeof(userid));
+    rc = write_cmd("ft_send %s\n", userid);
+    CU_ASSERT_FATAL(rc > 0);
+
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_send") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
+
+    // Wait for the ft_connect_cb to be invoked.
+    cond_wait(wctxt->ft_cond);
+
+    p = realpath(extra->file_name, path);
+    if (p) {
+        rc = remove(path);
+        CU_ASSERT_EQUAL(rc, 0);
+    }
+
+    fp_callbacks.state_changed = ft_state_changed_cb;
+    fp_callbacks.received = received_cb;
+    rc = ela_file_recv(wctxt->carrier, robotid, extra->file_name, &fp_callbacks, &test_context);
+    CU_ASSERT_EQUAL(rc, 0);
+
+    /* Wait for the ft_state_changed_cb to be invoked. After invocation,
+       the filetransfer connection state should have changed to be
+       FileTransferConnection_connecting. */
+    cond_wait(wctxt->ft_cond);
+
+    /* Wait for the ft_state_changed_cb to be invoked. After invocation,
+       the filetransfer connection state should have changed to be
+       FileTransferConnection_connected. */
+    cond_wait(wctxt->ft_cond);
+
+    ft_con_state_bits |= 1 << FileTransferConnection_connecting;
+    ft_con_state_bits |= 1 << FileTransferConnection_connected;
+    CU_ASSERT_EQUAL_FATAL(wctxt->ft_con_state_bits, ft_con_state_bits);
+
+    // Wait for the sender to finish sending.
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_send") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "done") == 0);
+
+    // Wait for the received_cb to be invoked.
+    cond_wait(wctxt->ft_cond);
+
+    fp = fopen(extra->file_name, "rb");
+    CU_ASSERT_PTR_NOT_NULL_FATAL(fp);
+    p = fgets(result, sizeof(result), fp);
+    CU_ASSERT_PTR_NOT_NULL_FATAL(p);
+    fclose(fp);
+
+    CU_ASSERT_TRUE(strcmp(data, result) == 0);
+
+    rc = write_cmd("ft_cleanup\n");
+    CU_ASSERT_FATAL(rc > 0);
+
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_cleanup") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
 
     ela_filetransfer_cleanup(wctxt->carrier);
 
@@ -233,10 +370,11 @@ static void test_filetransfer_file_resume_interrupted_transferring(void)
 {
     CarrierContext *wctxt = test_context.carrier;
     CarrierContextExtra *extra = wctxt->extra;
+    ElaFileProgressCallbacks fp_callbacks = {0};
+    FILE *fp = NULL;
     char userid[ELA_MAX_ID_LEN + 1] = {0};
     char cmd[32] = {0};
     char result[32] = {0};
-    char *p;
     const char *data = "abcd";
     uint8_t ft_con_state_bits = 0;
     int rc;
@@ -248,9 +386,11 @@ static void test_filetransfer_file_resume_interrupted_transferring(void)
     CU_ASSERT_TRUE_FATAL(ela_is_friend(wctxt->carrier, robotid));
 
     // Create a file to transfer.
-    sprintf(result, "printf %s > %s", data, extra->file_name);
-    rc = system(result);
-    CU_ASSERT_EQUAL_FATAL(rc, 0);
+    fp = fopen(extra->file_name, "w+b");
+    CU_ASSERT_PTR_NOT_NULL_FATAL(fp);
+    rc = fputs(data, fp);
+    CU_ASSERT_NOT_EQUAL_FATAL(rc, EOF);
+    fclose(fp);
 
     rc = ela_filetransfer_init(wctxt->carrier, NULL, &test_context);
     CU_ASSERT_EQUAL(rc, 0);
@@ -263,7 +403,9 @@ static void test_filetransfer_file_resume_interrupted_transferring(void)
     CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_init") == 0);
     CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
 
-    rc = ela_file_send(wctxt->carrier, robotid, extra->file_name, &fp_calllbacks, &test_context);
+    fp_callbacks.state_changed = ft_state_changed_cb;
+    fp_callbacks.sent = sent_cb;
+    rc = ela_file_send(wctxt->carrier, robotid, extra->file_name, &fp_callbacks, &test_context);
     CU_ASSERT_EQUAL(rc, 0);
 
     rc = read_ack("%32s %32s", cmd, result);
@@ -302,13 +444,140 @@ static void test_filetransfer_file_resume_interrupted_transferring(void)
     CU_ASSERT_TRUE_FATAL(strcmp(cmd, "fp_recv") == 0);
     CU_ASSERT_TRUE_FATAL(strcmp(result, "done") == 0);
 
-    rc = write_cmd("ft_res\n");
+    rc = write_cmd("ft_result\n");
     CU_ASSERT_FATAL(rc > 0);
 
     rc = read_ack("%32s %32s", cmd, result);
     CU_ASSERT_TRUE_FATAL(rc == 2);
-    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_res") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_result") == 0);
     CU_ASSERT_TRUE_FATAL(strcmp(result, data) == 0);
+
+    rc = write_cmd("ft_cleanup\n");
+    CU_ASSERT_FATAL(rc > 0);
+
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_cleanup") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
+
+    ela_filetransfer_cleanup(wctxt->carrier);
+
+    // Delete the file created before.
+    rc = remove(extra->file_name);
+    CU_ASSERT_EQUAL(rc, 0);
+}
+
+static void test_filetransfer_resume_receiving_file(void)
+{
+    CarrierContext *wctxt = test_context.carrier;
+    CarrierContextExtra *extra = wctxt->extra;
+    ElaFileProgressCallbacks fp_callbacks = {0};
+    FILE *fp = NULL;
+    char userid[ELA_MAX_ID_LEN + 1] = {0};
+    char tmp_file[512] = {0};
+    char cmd[32] = {0};
+    char result[32] = {0};
+    char path[PATH_MAX] = {0};
+    char *p;
+    const char *data = "abcd";
+    uint8_t ft_con_state_bits = 0;
+    int rc;
+
+    test_context.context_reset(&test_context);
+
+    rc = add_friend_anyway(&test_context, robotid, robotaddr);
+    CU_ASSERT_EQUAL_FATAL(rc, 0);
+    CU_ASSERT_TRUE_FATAL(ela_is_friend(wctxt->carrier, robotid));
+
+    strcat(tmp_file, extra->file_name);
+    strcat(tmp_file, ".ft~part");
+    fp = fopen(tmp_file, "w+b");
+    CU_ASSERT_PTR_NOT_NULL_FATAL(fp);
+    rc = fputs("ab", fp);
+    fclose(fp);
+    CU_ASSERT_NOT_EQUAL_FATAL(rc, EOF);
+
+    // Tell the robot to create a file to transfer.
+    rc = write_cmd("ft_file %s\n", data);
+    CU_ASSERT_FATAL(rc > 0);
+
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_file") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
+
+    rc = ela_filetransfer_init(wctxt->carrier, ft_connect_cb, &test_context);
+    CU_ASSERT_EQUAL(rc, 0);
+
+    rc = write_cmd("ft_init\n");
+    CU_ASSERT_FATAL(rc > 0);
+
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_init") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
+
+    ela_get_userid(wctxt->carrier, userid, sizeof(userid));
+    rc = write_cmd("ft_send %s\n", userid);
+    CU_ASSERT_FATAL(rc > 0);
+
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_send") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
+
+    // Wait for the ft_connect_cb to be invoked.
+    cond_wait(wctxt->ft_cond);
+
+    p = realpath(extra->file_name, path);
+    if (p) {
+        rc = remove(path);
+        CU_ASSERT_EQUAL(rc, 0);
+    }
+
+    fp_callbacks.state_changed = ft_state_changed_cb;
+    fp_callbacks.received = received_cb;
+    rc = ela_file_recv(wctxt->carrier, robotid, extra->file_name, &fp_callbacks, &test_context);
+    CU_ASSERT_EQUAL(rc, 0);
+
+    /* Wait for the ft_state_changed_cb to be invoked. After invocation,
+       the filetransfer connection state should have changed to be
+       FileTransferConnection_connecting. */
+    cond_wait(wctxt->ft_cond);
+
+    /* Wait for the ft_state_changed_cb to be invoked. After invocation,
+       the filetransfer connection state should have changed to be
+       FileTransferConnection_connected. */
+    cond_wait(wctxt->ft_cond);
+
+    ft_con_state_bits |= 1 << FileTransferConnection_connecting;
+    ft_con_state_bits |= 1 << FileTransferConnection_connected;
+    CU_ASSERT_EQUAL_FATAL(wctxt->ft_con_state_bits, ft_con_state_bits);
+
+    // Wait for the sender to finish sending.
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_send") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "done") == 0);
+
+    // Wait for the received_cb to be invoked.
+    cond_wait(wctxt->ft_cond);
+
+    fp = fopen(extra->file_name, "rb");
+    CU_ASSERT_PTR_NOT_NULL_FATAL(fp);
+    p = fgets(result, sizeof(result), fp);
+    CU_ASSERT_PTR_NOT_NULL_FATAL(p);
+    fclose(fp);
+
+    CU_ASSERT_TRUE(strcmp(data, result) == 0);
+
+    rc = write_cmd("ft_cleanup\n");
+    CU_ASSERT_FATAL(rc > 0);
+
+    rc = read_ack("%32s %32s", cmd, result);
+    CU_ASSERT_TRUE_FATAL(rc == 2);
+    CU_ASSERT_TRUE_FATAL(strcmp(cmd, "ft_cleanup") == 0);
+    CU_ASSERT_TRUE_FATAL(strcmp(result, "succeeded") == 0);
 
     ela_filetransfer_cleanup(wctxt->carrier);
 
@@ -318,8 +587,10 @@ static void test_filetransfer_file_resume_interrupted_transferring(void)
 }
 
 static CU_TestInfo cases[] = {
-    { "test_filetransfer_file", test_filetransfer_file },
-    { "test_filetransfer_file_resume_interrupted_transferring", test_filetransfer_file_resume_interrupted_transferring },
+    //{ "test_filetransfer_file", test_filetransfer_file },
+    { "test_filetransfer_receive_file", test_filetransfer_receive_file },
+    //{ "test_filetransfer_file_resume_interrupted_transferring", test_filetransfer_file_resume_interrupted_transferring },
+    { "test_filetransfer_resume_receiving_file", test_filetransfer_resume_receiving_file },
     { NULL, NULL }
 };
 
