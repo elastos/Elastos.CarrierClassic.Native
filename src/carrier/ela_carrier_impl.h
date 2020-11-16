4a6b3e0758a70321ce3b81d119820e67efd14646
lifenxiang <noahli2013@outlook.com>
2020-11-16 10:01:46 +0800

    CU-55uh71: revert "Support with carrier extension especially used for webrtc  (#339)"

--- src/carrier/ela_carrier_impl.h
+++ src/carrier/ela_carrier_impl.h
@@ -108,7 +108,6 @@ struct ElaCarrier {
     pthread_mutex_t ext_mutex;
     void *session;          //reserved for session extension.
     void *filetransfer;     //reserved for filetransfer extension.
-    void *carrier_extesion; //reserved for carrier extension.
 
     DHT dht;
 
@@ -165,17 +164,6 @@ typedef struct SessionExtension {
 
     uint8_t                 reserved[1];
 } SessionExtension;
-
-static const char *carrier_extension_name = "carrier";
-typedef struct CarrierExtension {
-    ElaCarrier              *carrier;
-
-    friend_invite_callback  friend_invite_cb;
-    void                    *friend_invite_context;
-
-    void                    *user_callback;
-    void                    *user_context;
-} CarrierExtension;
 
 CARRIER_API
 int ela_leave_all_groups(ElaCarrier *w);
